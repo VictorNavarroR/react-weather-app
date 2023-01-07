03cf117fdb834d3606a59e61601db0d2631d222a
Victor <victor.navarro@bootcamp-upgrade.com>
2023-01-07 17:39:49 +0100

    change http to https apiurl

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ interface Config {
 }
 
 export const config: Config = {
-    apiUrl: 'http://api.openweathermap.org/data/2.5/weather?',
-    apiForecastUrl: 'http://api.openweathermap.org/data/2.5/forecast?',
+    apiUrl: 'https://api.openweathermap.org/data/2.5/weather?',
+    apiForecastUrl: 'https://api.openweathermap.org/data/2.5/forecast?',
     apiKey: '967b7b1a5ff5c95858646f18e302fdb5',
     imagesApiUrl: 'https://api.pexels.com/v1/search?query=',
     imagesApiKey: '563492ad6f91700001000001257edbb45ce8446c82b338f4058f03cb'
